cf2fffb486ef70450cae9a563e6f77ba0f80ddfb
MatMasIt <onlinehomework@altervista.org>
2022-02-06 17:36:16 +0100

    Added dati on homepage

--- server/index.php
+++ server/index.php
@@ -201,7 +201,7 @@ function main($pdo)
     </footer>
 
     <footer class="w3-container w3-theme-d5">
-      <p>Repo <a href="<?php echo $GLOBALS["repoUrl"]; ?>" target="_blank">github</a></p>
+      <p>Repo <a href="<?php echo $GLOBALS["repoUrl"]; ?>" target="_blank">github</a> |   <a href="open.php"> Scarica i dati</a></p>
     </footer>
 
     <script>
